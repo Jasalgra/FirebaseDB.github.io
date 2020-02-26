e27a2e5fa9b41cc61f3d028536c93223ea2103de
Jasalgra <60706998+Jasalgra@users.noreply.github.com>
2020-02-26 09:07:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyByGRzvYTCA9GjHpVTnBy1idGBESB1JmEQ",
+    authDomain: "rest-apiwdb.firebaseapp.com",
+    databaseURL: "https://rest-apiwdb.firebaseio.com",
+    projectId: "rest-apiwdb",
+    storageBucket: "rest-apiwdb.appspot.com",
+    messagingSenderId: "528249096849",
+    appId: "1:528249096849:web:0bbcc6dcd24dde4ed58d30"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+// </script>
